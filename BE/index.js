a9e4a1814dba07bb62c0740879541656ce5999ad
BUICUONGG <cuongbb14@gmail.com>
2025-01-22 22:30:08 +0700

    .....

--- BE/index.js
+++ BE/index.js
@@ -8,8 +8,8 @@ app.listen(port, () => {
   console.log("Your app running on port " + `${port}`);
 });
 
-const uri = "mongodb+srv://cuongbui10704:cuongbui10704@swp.lg53w.mongodb.net/";
-
+const uri =
+  "mongodb+srv://cuongbui10704:cuongbui10704@swp.lg53w.mongodb.net/?retryWrites=true&w=majority&appName=SWP";
 app.get("/", async (req, res) => {
   await mongoose.connect(uri);
 
