8626f85cd1e677154db0aab2d91e19fb2b401424
BUICUONGG <cuongbb14@gmail.com>
2025-02-09 19:43:27 +0700

    testShowall User

--- FE/src/testUsers.jsx
+++ FE/src/testUsers.jsx
@@ -2,61 +2,59 @@ import { Table } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-
-
 const TestUsers = () => {
-    const [userList, setUserList] = useState([]);
+  const [userList, setUserList] = useState([]);
 
+  const fetchUsers = async () => {
+    const respone = await axios.get("http://localhost:8080/user/showData");
+    console.log(respone.data);
+    setUserList(respone.data);
+  };
 
-    const fetchUsers = async () => {
-        const respone = await axios.get("https://6794b828aad755a134ea3e5d.mockapi.io/User");
-        console.log(respone.data);
-        setUserList(respone.data);
-    };
-    
-    useEffect(() => {
-        fetchUsers();
-    }, []);
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
-
-    const columns = [
-        {
-            title: "UserID",
-            dataIndex: "id",
-            key: "id",
-        },
-        {
-            title: "UserName",
-            dataIndex: "name",
-            key: "name",
-        },
-        {
-            title: "Email",
-            dataIndex: "type",
-            key: "type",
-        },
-        {
-            title: "Phone",
-            dataIndex: "quantity",
-            key: "quantity",
-        },
-        {
-            title: "Password",
-            dataIndex: "password",
-            key: "password",
-            render: (password) => <p>{password}</p>,
-        },  
-    ];
-
+  const columns = [
+    {
+      title: "UserID",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "UserName",
+      dataIndex: "username",
+      key: "username",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Phone",
+      dataIndex: "phone",
+      key: "phone",
+    },
+    {
+      title: "Password",
+      dataIndex: "password",
+      key: "password",
+      render: (password) => <p>{password}</p>,
+    },
+    {
+      title: "role",
+      dataIndex: "role",
+      key: "role",
+    },
+  ];
 
   return (
     <div>
-        <h1>User Management</h1>
-        <Table dataSource={userList} columns={columns} />
-
-        
+      <h1>User Management</h1>
+      <Table dataSource={userList} columns={columns} />
     </div>
   );
-}
+};
 
 export default TestUsers;
