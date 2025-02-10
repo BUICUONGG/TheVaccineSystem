8515850cd259938ae083494e2d09fb80ab078f43
BUICUONGG <cuongbb14@gmail.com>
2025-02-10 16:23:12 +0700

    try catch

--- BE/src/services/vaccine.services.js
+++ BE/src/services/vaccine.services.js
@@ -3,23 +3,27 @@ import VaccineInventory from "../model/vaccineInventorySchema.js";
 import "dotenv/config";
 
 class VaccineService {
-    async addVaccine(vaccineData) {
-        try {
-            const vaccine = new VaccineInventory(vaccineData);
-            await vaccine.validate();
-            const result = await connectToDatabase.vaccines.insertOne(vaccine);
-            return { _id: result.insertedId, ...vaccineData };
-        } catch (error) {
-            console.error("Error adding vaccine:", error.message);
-            throw new Error(error.message);
-        }
+  async addVaccine(vaccineData) {
+    try {
+      const vaccine = new VaccineInventory(vaccineData);
+      await vaccine.validate();
+      const result = await connectToDatabase.vaccines.insertOne(vaccine);
+      return { _id: result.insertedId, ...vaccineData };
+    } catch (error) {
+      console.error("Error adding vaccine:", error.message);
+      throw new Error(error.message);
     }
+  }
 
-    async getVaccines() {
-        const vaccines = await connectToDatabase.vaccines.find().toArray();
-        return vaccines;
+  async getVaccines() {
+    try {
+      const vaccines = await connectToDatabase.vaccines.find().toArray();
+      return vaccines;
+    } catch (error) {
+      console.error("Error get vaccine:", error.message);
+      throw new Error(error.message);
     }
-
+  }
 }
 
 const vaccineService = new VaccineService();
