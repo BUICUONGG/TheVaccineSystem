d417dd8421eb917ecd946ff47bee6389d251b878
baole405 <baolttse184569@fpt.edu.vn>
2025-02-14 16:11:01 +0700

    vaccine controller edit

--- BE/src/controllers/vaccines.controllers.js
+++ BE/src/controllers/vaccines.controllers.js
@@ -3,9 +3,12 @@ import vaccineService from "../services/vaccine.services.js";
 export const addVaccineController = async (req, res) => {
   try {
     const vaccine = await vaccineService.addVaccine(req.body);
-    res.status(201).json("Vaccine added successfully", vaccine);
+    res.status(201).json({
+      message: "Vaccine added successfully",
+      vaccine: vaccine
+    });
   } catch (error) {
-    res.status(400).json(error.message);
+    res.status(400).json({ error: error.message });
   }
 };
 
