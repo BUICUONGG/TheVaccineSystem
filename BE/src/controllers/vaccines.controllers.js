6a514933b0400e6827a7f4aa052ce01dd27c04b6
BUICUONGG <cuongbb14@gmail.com>
2025-02-21 08:35:26 +0700

    ...

--- BE/src/controllers/vaccines.controllers.js
+++ BE/src/controllers/vaccines.controllers.js
@@ -65,7 +65,7 @@ export const showInfoVaccineController = async (req, res) => {
 
 export const showVaccineAndImportController = async (req, res) => {
   try {
-    const list = await vaccineService.getVaccineWithImports();
+    const list = await vaccineService.getVaccineWithImportsDetail();
     return res.status(200).json(list);
   } catch (error) {
     res.status(400).json({
